b3e5c267cacf005b9ee19ae712dc00520b41e2f0
George Zakharov <george_zakharov@mail.ru>
2022-01-17 23:56:12 +0300

    Base models add

--- sn_app/base/views.py
+++ sn_app/base/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
+from .models import Room
 # TODO: FOR TEST
 # from django.http import HttpResponse
 
-rooms = [
-    {'id': 1, 'name':'Lets learn python'},
-    {'id': 2, 'name':'Lets learn design'},
-    {'id': 3, 'name':'Lets learn devops'},
-]
+# TODO: TEST
+# rooms = [
+#     {'id': 1, 'name':'Lets learn python'},
+#     {'id': 2, 'name':'Lets learn design'},
+#     {'id': 3, 'name':'Lets learn devops'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
     # TODO: FOR TEST
     # return HttpResponse('Home page')
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
     # TODO: FOR TEST
