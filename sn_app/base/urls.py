e51b60bb840291f7f286ce80f63da7fc7b72dec4
George Zakharov <george_zakharov@mail.ru>
2022-01-25 22:09:39 +0300

    Login-logout procedure update

--- sn_app/base/urls.py
+++ sn_app/base/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create_room/', views.create_room, name="create-room"),
     path('update_room/<str:pk>/', views.update_room, name="update-room"),
